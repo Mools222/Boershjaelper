ed13bdfcce8a559bcec6854fd817a490d625ffa8
Morten <morten222@hotmail.com>
2019-03-16 19:31:29 +0100

    Refactoring.

--- app/src/main/java/com/example/time18_boershjaelper/MainActivity.java
+++ app/src/main/java/com/example/time18_boershjaelper/MainActivity.java
@@ -1,11 +1,7 @@
 package com.example.time18_boershjaelper;
 
-import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.os.Build;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -23,34 +19,34 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
-    private EditText ETantalAktier;
-    private EditText ETkoebskurs;
-    private EditText ETkurtage;
+    private EditText editTextAntalAktier;
+    private EditText editTextKoebskurs;
+    private EditText editTextKurtage;
 
-    private TextView TWbreakeven;
-    private TextView TWen;
-    private TextView TWfem;
+    private TextView textViewBreakeven;
+    private TextView textViewEn;
+    private TextView textViewFem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ETantalAktier = findViewById(R.id.editTextAA);
-        ETkoebskurs = findViewById(R.id.editTextKK);
-        ETkurtage = findViewById(R.id.editTextK);
+        editTextAntalAktier = findViewById(R.id.editTextAA);
+        editTextKoebskurs = findViewById(R.id.editTextKK);
+        editTextKurtage = findViewById(R.id.editTextK);
 
-        TWbreakeven = findViewById(R.id.textViewBE);
-        TWen = findViewById(R.id.textViewEn);
-        TWfem = findViewById(R.id.textViewFem);
+        textViewBreakeven = findViewById(R.id.textViewBE);
+        textViewEn = findViewById(R.id.textViewEn);
+        textViewFem = findViewById(R.id.textViewFem);
 
         TextWatcher watcher = new TextWatcher() {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 try {
-                    double aa = Double.parseDouble(ETantalAktier.getText().toString());
-                    double kk = Double.parseDouble(ETkoebskurs.getText().toString());
-                    double k = Double.parseDouble(ETkurtage.getText().toString());
+                    double aa = Double.parseDouble(editTextAntalAktier.getText().toString());
+                    double kk = Double.parseDouble(editTextKoebskurs.getText().toString());
+                    double k = Double.parseDouble(editTextKurtage.getText().toString());
 
                     double be = Kurvekseler.breakeven(aa, kk, k);
                     double en = Kurvekseler.tjentEn(aa, kk, k);
@@ -60,13 +56,13 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
                     String formatEn = String.format(Locale.ENGLISH, "%4.2f", en);
                     String formatFem = String.format(Locale.ENGLISH, "%4.2f", fem);
 
-                    TWbreakeven.setText(formatBE);
-                    TWen.setText(formatEn);
-                    TWfem.setText(formatFem);
+                    textViewBreakeven.setText(formatBE);
+                    textViewEn.setText(formatEn);
+                    textViewFem.setText(formatFem);
 
-//                    TWbreakeven.setText(String.valueOf(be));
-//                    TWen.setText(String.valueOf(en));
-//                    TWfem.setText(String.valueOf(fem));
+//                    textViewBreakeven.setText(String.valueOf(be));
+//                    textViewEn.setText(String.valueOf(en));
+//                    textViewFem.setText(String.valueOf(fem));
 
                 } catch (NumberFormatException e) {
                     System.out.println("Nope1");
@@ -82,17 +78,17 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
             public void afterTextChanged(Editable s) { }
         };
 
-        ETantalAktier.addTextChangedListener(watcher);
-        ETkoebskurs.addTextChangedListener(watcher);
-        ETkurtage.addTextChangedListener(watcher);
+        editTextAntalAktier.addTextChangedListener(watcher);
+        editTextKoebskurs.addTextChangedListener(watcher);
+        editTextKurtage.addTextChangedListener(watcher);
 
         setupSharedPreferences();
     }
 
     public void onClickShareTextButton(View v) {
-        String beregning = "Breakeven ved kurs: " + TWbreakeven.getText().toString() + "\n" +
-                "Tjent 1% ved kurs: " + TWen.getText().toString() + "\n" +
-                "Tjent 5% ved kurs: " + TWfem.getText().toString();
+        String beregning = "Breakeven ved kurs: " + textViewBreakeven.getText().toString() + "\n" +
+                "Tjent 1% ved kurs: " + textViewEn.getText().toString() + "\n" +
+                "Tjent 5% ved kurs: " + textViewFem.getText().toString();
         shareText(beregning);
     }
 
@@ -110,11 +106,8 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        /* Use AppCompatActivity's method getMenuInflater to get a handle on the menu inflater */
         MenuInflater inflater = getMenuInflater();
-        /* Use the inflater's inflate method to inflate our visualizer_menu layout to this menu */
         inflater.inflate(R.menu.menu, menu);
-        /* Return true so that the visualizer_menu is displayed in the Toolbar */
         return true;
     }
 
@@ -135,13 +128,13 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
 
         String aaString = sharedPreferences.getString(getString(R.string.pref_antalAktier_key), getString(R.string.pref_antalAktier_default));
 //        float aaFloat = Float.parseFloat(aaString);
-        ETantalAktier.setText(aaString);
+        editTextAntalAktier.setText(aaString);
 
         String kk = sharedPreferences.getString(getString(R.string.pref_koebskurs_key), getString(R.string.pref_koebskurs__default));
-        ETkoebskurs.setText(kk);
+        editTextKoebskurs.setText(kk);
 
         String k = sharedPreferences.getString(getString(R.string.pref_kurtage_key), getString(R.string.pref_kurtage_default));
-        ETkurtage.setText(k);
+        editTextKurtage.setText(k);
 
 //        float minSize = Float.parseFloat(sharedPreferences.getString(getString(R.string.pref_size_key),
 //                getString(R.string.pref_size_default)));
@@ -155,15 +148,13 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         if (key.equals(getString(R.string.pref_antalAktier_key))) {
             String aaString = sharedPreferences.getString(key, getString(R.string.pref_antalAktier_default));
-            ETantalAktier.setText(aaString);
+            editTextAntalAktier.setText(aaString);
         } else if (key.equals(getString(R.string.pref_koebskurs_key))) {
             String kk = sharedPreferences.getString(key, getString(R.string.pref_koebskurs__default));
-            ETkoebskurs.setText(kk);
+            editTextKoebskurs.setText(kk);
         } else if (key.equals(getString(R.string.pref_kurtage_key))) {
             String k = sharedPreferences.getString(key, getString(R.string.pref_kurtage_default));
-            ETkurtage.setText(k);
+            editTextKurtage.setText(k);
         }
     }
-
-
 }
--- app/src/main/java/com/example/time18_boershjaelper/SettingsFragment.java
+++ app/src/main/java/com/example/time18_boershjaelper/SettingsFragment.java
@@ -1,7 +1,6 @@
 package com.example.time18_boershjaelper;
 
 import android.content.SharedPreferences;
-import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.support.v7.preference.CheckBoxPreference;
 import android.support.v7.preference.EditTextPreference;
@@ -22,11 +21,9 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
         PreferenceScreen prefScreen = getPreferenceScreen();
         int count = prefScreen.getPreferenceCount();
 
-        // Go through all of the preferences, and set up their preference summary.
         for (int i = 0; i < count; i++) {
             Preference p = prefScreen.getPreference(i);
-            // You don't need to set up preference summaries for checkbox preferences because
-            // they are already set up in xml using summaryOff and summary On
+
             if (!(p instanceof CheckBoxPreference)) {
                 String value = sharedPreferences.getString(p.getKey(), "");
                 setPreferenceSummary(p, value);
@@ -35,8 +32,6 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
             }
         }
 
-        // COMPLETED (3) Add the OnPreferenceChangeListener specifically to the EditTextPreference
-        // Add the preference listener which checks that the size is correct to the size preference
         Preference preference1 = findPreference(getString(R.string.pref_antalAktier_key));
         preference1.setOnPreferenceChangeListener(this);
 
@@ -45,15 +40,13 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
 
         Preference preference3 = findPreference(getString(R.string.pref_kurtage_key));
         preference3.setOnPreferenceChangeListener(this);
-
     }
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        // Figure out which preference was changed
         Preference preference = findPreference(key);
+
         if (null != preference) {
-            // Updates the summary for the preference
             if (!(preference instanceof CheckBoxPreference)) {
                 String value = sharedPreferences.getString(preference.getKey(), "");
                 setPreferenceSummary(preference, value);
@@ -61,43 +54,25 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
         }
     }
 
-    /**
-     * Updates the summary for the preference
-     *
-     * @param preference The preference to be updated
-     * @param value      The value that the preference was updated to
-     */
     private void setPreferenceSummary(Preference preference, String value) {
         if (preference instanceof ListPreference) {
-            // For list preferences, figure out the label of the selected value
             ListPreference listPreference = (ListPreference) preference;
             int prefIndex = listPreference.findIndexOfValue(value);
+
             if (prefIndex >= 0) {
-                // Set the summary to that label
                 listPreference.setSummary(listPreference.getEntries()[prefIndex]);
             }
         } else if (preference instanceof EditTextPreference) {
-            // For EditTextPreferences, set the summary to the value's simple string representation.
             preference.setSummary(value);
         }
     }
 
-    // COMPLETED (2) Override onPreferenceChange. This method should try to convert the new preference value
-    // to a float; if it cannot, show a helpful error message and return false. If it can be converted
-    // to a float check that that float is between 0 (exclusive) and 3 (inclusive). If it isn't, show
-    // an error message and return false. If it is a valid number, return true.
-
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        // In this context, we're using the onPreferenceChange listener for checking whether the
-        // size setting was set to a valid value.
-
-        Toast error = Toast.makeText(getContext(), "Please select a number between 0.1 and 3", Toast.LENGTH_SHORT);
 
         Toast error1 = Toast.makeText(getContext(), "Indtast heltal", Toast.LENGTH_SHORT);
         Toast error2 = Toast.makeText(getContext(), "Indtast hel- eller kommatal", Toast.LENGTH_SHORT);
 
-        // Double check that the preference is the size preference
         String antalAktierKey = getString(R.string.pref_antalAktier_key);
         String koebskursKey = getString(R.string.pref_koebskurs_key);
         String kurtageKey = getString(R.string.pref_kurtage_key);
@@ -107,7 +82,6 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
         if (preference.getKey().equals(antalAktierKey)) {
             try {
                 float size = Float.parseFloat(stringSize);
-                // If the number is outside of the acceptable range, show an error.
                 if (size % 1 != 0) {
                     error1.show();
                     return false;
